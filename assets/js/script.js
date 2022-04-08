1d6687ad5d2fa8577e54c6b588316a5e5a3f5e62
jemm2008 <jemm2008@gmail.com>
2022-04-08 16:22:41 -0400

    Se añadió funcionalidad a la barra de navegación

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,20 @@ $(function(){
         $(".card").toggle();
         $(this).parents(".card").toggle();
     })
-
+    
+    // Codigo JavaScript para transparentar o Colorear la Barra de Navegación--
+    // Esta echo según el evento Scroll, 
+    // y toma el valor de trigger según el "ancho del ViewPort"--
+    $(window).scroll(function(){        
+        const viewport_width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        if($("#barra_menu").offset().top > (viewport_width/1.67)){
+            $("#barra_menu").removeClass("bg-transparent");
+            $("#barra_menu").addClass("barra_turquesa");
+        }
+        else{
+            $("#barra_menu").removeClass("barra_turquesa");
+            }
+        })
 
 });
 
